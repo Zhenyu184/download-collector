20fc3cc404a8b8bebe430dc04fb9faeaa94f65c6
Zhenyu <zyy1120126@gmail.com>
2024-05-19 19:39:45 +0800

    feart: detect file extension name

--- background.js
+++ background.js
@@ -1,9 +1,24 @@
 chrome.downloads.onCreated.addListener((downloadItem) => {
-    console.log("Download started:", downloadItem);
+    console.log("Download Created ", downloadItem);
 });
 
 chrome.downloads.onChanged.addListener((delta) => {
     console.log("Download changed:", delta);
+    try {
+        const filePath = delta.filename.current;
+        if (!filePath) return;
+
+        const lastDotIndex = filePath.lastIndexOf(".");
+        if (lastDotIndex === -1) return;
+
+        const extensionName = filePath.substring(lastDotIndex + 1);
+        if (!extensionName) return;
+
+        console.log("extension mame is > ", extensionName);
+    } catch (error) {
+        // console.log(error);
+    }
+    return;
 });
 
 chrome.downloads.onErased.addListener((downloadId) => {
