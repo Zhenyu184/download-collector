333e3c09ae5e78b858872833889510501416864b
Zhenyu <zyy1120126@gmail.com>
2024-05-19 20:44:27 +0800

    feat: written while drunk

--- background.js
+++ background.js
@@ -1,9 +1,8 @@
 chrome.downloads.onCreated.addListener((downloadItem) => {
-    console.log("Download Created ", downloadItem);
+    console.log("created ", downloadItem);
 });
 
 chrome.downloads.onChanged.addListener((delta) => {
-    console.log("Download changed:", delta);
     try {
         const filePath = delta.filename.current;
         if (!filePath) return;
@@ -14,13 +13,81 @@ chrome.downloads.onChanged.addListener((delta) => {
         const extensionName = filePath.substring(lastDotIndex + 1);
         if (!extensionName) return;
 
-        console.log("extension mame is > ", extensionName);
+        // console.log("extension mame is > ", extensionName);
+    } catch (error) {}
+
+    try {
+        const downloadState = delta.state;
+        if (!downloadState) return;
+
+        const downloadCurrent = downloadState.current;
+        if (downloadCurrent !== "complete") return;
+
+        console.log("download complete ");
+
+        chrome.downloads.search({ id: delta.id }, (results) => {
+            if (results.length <= 0) return;
+
+            const downloadItem = results[0];
+            if (!downloadItem) return;
+
+            const fullFilePath = downloadItem.filename;
+            if (!fullFilePath) return;
+
+            const lastDotIndex = fullFilePath.lastIndexOf(".");
+            if (!lastDotIndex) return;
+
+            const extensionName = fullFilePath.substring(lastDotIndex + 1);
+            if (!extensionName) return;
+
+            console.log("fullFilePath ", fullFilePath);
+            console.log("extension mame is > ", extensionName);
+
+            if (extensionName === "webp") {
+                const newFilePath = `photo/test.webp`;
+                moveFile(downloadItem.url, newFilePath, downloadItem.id);
+            }
+        });
     } catch (error) {
-        // console.log(error);
+        console.log(error);
     }
     return;
 });
 
-chrome.downloads.onErased.addListener((downloadId) => {
-    console.log("Download erased:", downloadId);
+chrome.downloads.onDeterminingFilename.addListener((downloadId) => {
+    console.log("onDeterminingFilename:", downloadId);
 });
+
+chrome.downloads.onErased.addListener((downloadId) => {
+    console.log("onErased:", downloadId);
+});
+
+// function moveFile(url, newFilePath, originalDownloadId) {
+//     chrome.downloads.download(
+//         {
+//             url: url,
+//             filename: newFilePath,
+//             conflictAction: "overwrite",
+//         },
+//         (newDownloadId) => {
+//             if (chrome.runtime.lastError) {
+//                 console.error(
+//                     "Error downloading file:",
+//                     chrome.runtime.lastError
+//                 );
+//             } else {
+//                 // 刪除原始文件
+//                 chrome.downloads.erase({ id: originalDownloadId }, () => {
+//                     if (chrome.runtime.lastError) {
+//                         console.error(
+//                             "Error erasing original file:",
+//                             chrome.runtime.lastError
+//                         );
+//                     } else {
+//                         console.log(`File moved to ${newFilePath}`);
+//                     }
+//                 });
+//             }
+//         }
+//     );
+// }
